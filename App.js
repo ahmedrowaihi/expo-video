6e65ef5b732fffb49e1c3035ceafa3843baf75d5
ahmedrowaihi <ahmedrowaihi1@gmail.com>
2021-08-06 05:25:29 +0300

    expo video

--- App.js
+++ App.js
@@ -1,21 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] =
+    useState(null);
+  const [camera, setCamera] = useState(null);
+  const [record, setRecord] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+
+      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+      setHasAudioPermission(audioStatus.status === "granted");
+      const Media = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaLibraryPermission(Media.status === "granted");
+    })();
+  }, []);
+
+  const takeVideo = async () => {
+    if (camera) {
+      const data = await camera.recordAsync({
+        maxDuration: 10,
+      });
+      setRecord(data.uri);
+      MediaLibrary.saveToLibraryAsync(data.uri);
+      console.log(data.uri);
+    }
+  };
+
+  const stopVideo = async () => {
+    camera.stopRecording();
+  };
+
+  if (hasCameraPermission === null || hasAudioPermission === null) {
+    return <View />;
+  }
+  if (
+    hasCameraPermission === false ||
+    hasAudioPermission === false ||
+    hasMediaLibraryPermission === false
+  ) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={"4:3"}
+        />
+      </View>
+      <View
+        onTouchStart={takeVideo}
+        onTouchEnd={stopVideo}
+        style={{
+          position: "absolute",
+          backgroundColor: "black",
+          width: 500,
+          height: 1000,
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  cameraContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  video: {
+    alignSelf: "center",
+    width: 350,
+    height: 220,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
